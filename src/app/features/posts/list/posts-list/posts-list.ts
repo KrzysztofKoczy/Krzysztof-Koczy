49193ff0ed769ec58fbbb5809753327d73f2f048
Krzysztof <krzysztof.koczy99@gmail.com>
2025-09-25 09:34:59 +0200

    create posts list

--- src/app/features/posts/list/posts-list/posts-list.ts
+++ src/app/features/posts/list/posts-list/posts-list.ts
@@ -1,11 +1,32 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, OnInit, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { PostDto, PostsRest } from '../../../../services/posts-rest';
 
 @Component({
   selector: 'app-posts-list',
   standalone: true,
   imports: [],
   templateUrl: './posts-list.html',
+  styleUrls: ['./posts-list.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PostsList {
+export class PostsList implements OnInit {
+  private readonly postsRest = inject(PostsRest);
+  private readonly destroyRef = inject(DestroyRef);
+
+  posts = signal<PostDto[]>([]);
+
+  ngOnInit(): void {
+    this.loadPosts();
+  }
+
+  private loadPosts(): void {
+    this.postsRest
+      .getPosts()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((posts) => { 
+        console.log('Posts:', posts)
+        this.posts.set(posts);  
+      });
+  }
 }
